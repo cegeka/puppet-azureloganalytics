291e00b22f5ab1a1d7f5d492ecb1bd624dc83e99
Fabian Dammekens <fabian@dammekens.be>
2023-04-27 10:06:43 +0200

    Refactor service to use file resource instead of exec

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,9 +3,9 @@
 # This is a private class and should not be called directly
 #
 class azureloganalytics::service (
-  String  $service_ensure   = $azureloganalytics::service_ensure,
-  Array   $service_name     = $azureloganalytics::service_name,
-  Boolean $service_enable  = $azureloganalytics::service_enable
+  String  $service_ensure = $azureloganalytics::service_ensure,
+  Array   $service_name   = $azureloganalytics::service_name,
+  Boolean $service_enable = $azureloganalytics::service_enable
 ) {
 
 
@@ -14,22 +14,14 @@ class azureloganalytics::service (
     enable  => $service_enable,
     require => [
       Class['azureloganalytics::package'],
-      Exec['symlink-omsagent-service']
+      File['/etc/systemd/system/omsagent.service']
     ]
   }
 
   # By default a service is created with the UUID in its name, link this to a service called omsagent
-  exec { 'symlink-omsagent-service':
-    command => 'ln -s /usr/lib/systemd/system/omsagent-*.service /etc/systemd/system/omsagent.service',
-    path    => [ '/usr/bin', '/bin', '/usr/sbin' ],
-    creates => '/etc/systemd/system/omsagent.service',
-    notify  => Exec['azureloganalytics-systemd-reload']
-  }
-
-  exec { 'azureloganalytics-systemd-reload':
-    command     => 'systemctl daemon-reload',
-    path        => [ '/usr/bin', '/bin', '/usr/sbin' ],
-    refreshonly => true
+  file { '/etc/systemd/system/omsagent.service':
+    ensure => link,
+    target => "/usr/lib/systemd/system/omsagent-${azureloganalytics::workspaceid}.service"
   }
 
 }
